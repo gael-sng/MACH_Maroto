78c65be4992e88452b7fa150aaced150845fbd23
CristianoChiaramelli <cristianochiaramelli@gmail.com>
2016-12-04 18:12:43 -0200

    Commit com as parada de tiro novas

---
+++ Assets/Scripts/EnemyShooting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : Shooting {
+
+    [Header("Bullet damage")]
+    public new float bulletDamage;
+    [Header("Secs per shot")]
+    public new float shootingInterval;
+    [Header("Bullet speed")]
+    public new float bulletSpeed;
+    [Header("Bullet Prefab")]
+    public new GameObject bulletPrefab;
+
+    private GameObject barrel;
+
+    void Start() {
+        foreach (Transform child in transform)
+            if (child.tag == "Barrel")
+                barrel = child.gameObject;
+    }
+
+    protected override void Shoot() {
+        ShootFromBarrel(barrel);
+    }
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,61 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Shooting : MonoBehaviour {
+public abstract class Shooting : MonoBehaviour {
 
-    [Header("Bullets prefab")]
-    public bulletType[] bulletTypes;
+    protected float timer = 0.0f;
 
-    [Header("Gun barrel")]
-    public GameObject barrel;
+    protected float bulletDamage;
+    protected float shootingInterval;
+    protected float bulletSpeed;
+    protected GameObject bulletPrefab;
 
+    // Update is called once per frame
+    void Update() {
 
-    [System.Serializable]
-    public struct bulletType {
-        [Header("Bullet damage")]
-        public float bulletDamage;
-        [Header("Secs per shot")]
-        public float shootingInterval;
-        [Header("Bullet speed")]
-        public float bulletSpeed;
-        [Header("Bullet Prefab")]
-        public GameObject bulletPrefab;
-    }
-    private bulletType actualBullet;
-    private float timer = 0.0f;
-    // Use this for initialization
-    void Start () {
-        actualBullet = bulletTypes[0];
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        
-        if (timer >= actualBullet.shootingInterval) {
+        if (timer >= shootingInterval) {
             timer = 0;
             Shoot();
         }
         timer += Time.deltaTime;
-	}
 
-    public void UpgradeBullet() {
-        for (int i=0; i< bulletTypes.Length; i++) {
-            if (bulletTypes[i].Equals(actualBullet)) { 
-                if (i+1 < bulletTypes.Length) {
-                    actualBullet = bulletTypes[i + 1];
-                } else {
-                    //Already has the last bullet type
-                }
-            }
-        }
+
     }
 
-    private void Shoot() {
-        Quaternion bulletRotation = Quaternion.Euler(Vector3.forward*barrel.transform.eulerAngles.z);
-        GameObject bulletInst = (GameObject) Instantiate(actualBullet.bulletPrefab, barrel.transform.position, bulletRotation);
+    protected abstract void Shoot();
 
-        bulletInst.GetComponent<bulletScript>().SetSpeed(actualBullet.bulletSpeed);
-        bulletInst.GetComponent<bulletScript>().SetDamage(actualBullet.bulletDamage);
+    protected void ShootFromBarrel(GameObject barrel) {
+        Quaternion bulletRotation = Quaternion.Euler(Vector3.forward*barrel.transform.eulerAngles.z);
+        GameObject bulletInst = (GameObject) Instantiate(bulletPrefab, barrel.transform.position, bulletRotation);
+
+        bulletInst.GetComponent<bulletScript>().SetSpeed(bulletSpeed);
+        bulletInst.GetComponent<bulletScript>().SetDamage(bulletDamage);
         bulletInst.GetComponent<bulletScript>().SetTag(gameObject.tag);
     }
 }
