61552f4d4698ef6d560a407308b2762cbbd69b5b
gael-sng <gael.sng@gmail.com>
2016-12-06 17:01:40 -0200

    Modifiquei os prefabs dos inimigos dando nome a eles e testei amudança de msucia do boss

--- Assets/Scripts/Enemy/spawnerBehavior.cs
+++ Assets/Scripts/Enemy/spawnerBehavior.cs
@@ -5,17 +5,24 @@ public class spawnerBehavior : Utilities {
 	[Header("Current player reference")]
 	public GameObject player;
 	[Header("Enemies prfabs:")]
-	public GameObject enemy_1;
-	public GameObject enemy_2;
-	public GameObject enemy_3;
-	public GameObject enemy_4;
+	public GameObject Stalkel;
+	public GameObject Campel;
+	public GameObject Tarkel;
+	public GameObject Harbingel;
 
 	public float spawnDelay;
 
+	//public PlayMusic audio;
+
 	private float timer;
+
+	private GameObject boss;
+	private bool isBossTime;
 
 	// Use this for initialization
 	void Start () {
+		isBossTime = false;
+		boss = null;
 		if (spawnDelay == 0)
 			spawnDelay = 1;
 	}
@@ -23,15 +30,23 @@ public class spawnerBehavior : Utilities {
 	// Update is called once per frame
 	void Update () {
 		if (player == null) return;
+		if (boss != null)
+			return;
+		else if (isBossTime) {
+			isBossTime = false;
+			Camera.main.GetComponent<PlayMusic> ().changeMusic();
+		}
+		
 		Vector3 newPosition = new Vector3 (Random.Range (GetMinHorizontalPosition (), GetMaxHorizontalPosition()), GetMaxVerticalPosition () + 0.5f, 0);
 		if (timer >= spawnDelay) {
 			timer = 0;
 			GameObject enemy;
+			print ("bora spawnar");
 			switch((int)Random.Range(1, 5)){
 			case 1:
 				//print ("case 1");
-				if (enemy_1) {
-					enemy = Instantiate (enemy_1);
+				if (Stalkel) {
+					enemy = Instantiate (Stalkel);
 
 					//Give to enemy a reference to the palyer
 					enemy.GetComponent<EnemyControl>().player = player.transform;
@@ -42,8 +57,8 @@ public class spawnerBehavior : Utilities {
 
 			case 2:
 				//print ("case 2");
-				if (enemy_2) {
-					enemy = Instantiate (enemy_2);
+				if (Campel) {
+					enemy = Instantiate (Campel);
 
 					//Give to enemy a reference to the palyer
 					enemy.GetComponent<CampelBehaviour> ().player = player.transform;
@@ -54,8 +69,8 @@ public class spawnerBehavior : Utilities {
 
 			case 3:
 				//print ("case 3");
-				if (enemy_3) {
-					enemy = Instantiate (enemy_3);
+				if (Tarkel) {
+					enemy = Instantiate (Tarkel);
 
 					//Give to enemy a reference to the palyer
 					enemy.GetComponent<TarkelBehaviour>().player = player;
@@ -68,13 +83,18 @@ public class spawnerBehavior : Utilities {
 
 			case 4:
 				//print ("case 4");
-				if (enemy_1) {
-					enemy = Instantiate (enemy_1);
+				if (Harbingel) {
+					print ("BOSS TIME");
+					Camera.main.GetComponent<PlayMusic> ().changeMusic ();
 
+					enemy = Instantiate (Harbingel);
+					boss = enemy;
 					//Give to enemy a reference to the palyer
 					enemy.GetComponent<EnemyControl>().player = player.transform;
+					enemy.GetComponent<BossBehaviour>().player = player;
 					//put the enemy in a random position above the screen
 					enemy.GetComponent<Transform> ().position = newPosition;
+					isBossTime = true;
 				}
 				break;
 
