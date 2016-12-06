57fab3a5673e652526833fb7537d29ccc17583df
CristianoChiaramelli <cristianochiaramelli@gmail.com>
2016-12-06 01:58:49 -0200

    Revert "Mais paradas"
    This reverts commit e321a8977aed859fd268cfd3b3ddd73249ba171b.

--- Assets/Scripts/Features/UpgradeScript.cs
+++ Assets/Scripts/Features/UpgradeScript.cs
@@ -5,30 +5,12 @@ public class UpgradeScript : MonoBehaviour {
 
     public float speed;
     private Vector2 dir;
-    private Color[] randomColorArray = { Color.white, Color.blue, Color.green, Color.red };
-    private Color colorA, colorB;
-    private int index;
-    private float counter;
 
     void Start() {
         dir = new Vector2(Random.value-0.5f, -2.0f).normalized * speed * Time.deltaTime;
-        index = Random.Range(0, 3);
-        colorA = randomColorArray[index];
-        colorB = randomColorArray[(++index) % 4];
-        counter = 0.0f;
     }
-
     // Update is called once per frame
     void Update () {
         transform.Translate(dir);
-
-        GetComponent<SpriteRenderer>().color = Color.Lerp(colorA, colorB, counter);
-        if (GetComponent<SpriteRenderer>().color == colorB) {
-            colorA = colorB;
-            colorB = randomColorArray[(++index) % 4];
-            counter = 0.0f;
-        }
-        counter += Time.deltaTime;
-
-    }
+	}
 }
