7a8ff09acd746959d98a5ec743908e4337d91384
Elisa Trujillo <elisa.trujillo@usp.br>
2016-12-06 09:00:08 -0200

    Playing around with boss theme

--- Assets/Scripts/Features/loopBackground.cs
+++ Assets/Scripts/Features/loopBackground.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//This class shifts the texture of the background in order to make it seem like its moving
 public class loopBackground : MonoBehaviour {
 
     public float speed=0;
-	// Update is called once per frame
+	
 	void Update () {
         GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0f, (Time.time*speed)%1);
 	}
